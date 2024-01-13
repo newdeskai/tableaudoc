cbc2f86c7a57c1d64a8ce117d29c6e49858a8052
powerbiodoo <151531194+powerbiodoo@users.noreply.github.com>
2024-01-13 18:05:39 +0530

    Add files via upload

---
+++ tableaudoc/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tableau Connector",
+
+    'summary': """
+       	  Integrate all your data using Tableau Odoo Connector. Works with Odoo Sh also. Tableu tablu tablue Power BI.
+       """,
+
+    'description': """
+       Integrate all your data using Tableau Odoo Connector. Works with Odoo Sh also. Tableu tablu tablue Power BI Tableau.
+    """,
+        
+    'maintainer': 'Tableau TN',
+    'author': "Tableau TN",
+    'website': "https://techneith.com/products/tableau-direct-connector/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '1.1',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/tableau-gif.gif'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
